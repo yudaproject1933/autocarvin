ce136b9bf55ea5ee808cf44d9ccc05ad15b44e2a
unknown <bambang.triyuda.permana@gmail.com>
2022-01-02 22:50:57 +0700

    env

--- resources/views/layouts/landing/content.blade.php
+++ resources/views/layouts/landing/content.blade.php
@@ -155,12 +155,12 @@
             <div class="maine_action">
                 <div class="col-md-8">
                     <div class="action_item text-center">
-                        <h2 class="text-white text-uppercase">Your Promotion Text Will Be Here</h2>
+                        <h2 class="text-white text-uppercase" style="font-size: 20pt;">What are you waiting for, Get your report now!!</h2>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="action_btn text-left sm-text-center">
-                        <a href="" class="btn btn-default">Purchase Now</a>
+                        <a href="#home" class="btn btn-default">Purchase Now</a>
                     </div>
                 </div>
             </div>
