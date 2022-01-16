758be4652ebccdf583ba0335d566d1ddabe54093
unknown <bambang.triyuda.permana@gmail.com>
2022-01-16 15:29:44 +0700

    email dan pp

--- resources/views/admin/dashboard/template_email.blade.php
+++ resources/views/admin/dashboard/template_email.blade.php
@@ -7,12 +7,16 @@
     <title>Email</title>
 </head>
 <body>
-    <span>Hi there, your recent order on history report has been completed.</span>
+    {{-- <span>Hi there, your recent order on history report has been completed.</span>
     <p>Your report has been sent to your email, please check your spam folder just in case the email got delivered there instead of your inbox.</p>
     <p>You can access the report through the link below :</p>
     {{ $details['link'] }}<br>
     <p>Ans we also attach your report here, if you are having trouble opening the link.</p>
     <br>
-    <span>If you have any question, you can reply this message to get in touch with us.</span>
+    <span>If you have any question, you can reply this message to get in touch with us.</span> --}}
+    <h4>Dear customer,</h4>
+    <p>Thanks for your order, now your completed report is available on the  link below:</p>
+    {{ $details['link'] }}<br>
+    <span>If you have any question , please just reply this message to get in touch with us.</span>
 </body>
 </html>
