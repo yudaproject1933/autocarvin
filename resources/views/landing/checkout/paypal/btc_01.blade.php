db920836ba1a1249225e4292a320bdc938c0539b
unknown <bambang.triyuda.permana@gmail.com>
2022-03-18 07:11:08 -0600

    btc

---
+++ resources/views/landing/checkout/paypal/btc_01.blade.php
@@ -0,0 +1,10 @@
+
+    <div>
+        <a class="buy-with-crypto"
+          href="https://commerce.coinbase.com/checkout/33880472-e7be-4a2f-8632-7df317354d19">
+          Pay With Crypto
+        </a>
+        <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807">
+        </script>
+      </div>
+  
